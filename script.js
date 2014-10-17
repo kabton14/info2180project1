5408d3c9429699d205ebbd6a5dc9fb02a2b060a6
YCmona <kabton14@gmail.com>
2014-10-17 14:02:56 -0500

    Second Upload of completed project

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function StaffMember(name,discountPercent){
-    this.name = name;
+window.onload = function () {
+	
+
+	//$("listbutton").onclick = list;
+	//$("ssearch").onclick = _search;
+	//$("addbutton").onclick = add;
+	
+	
+	$("scan1").onclick = cashRegister.scan;
+	$("void_button").onclick = cashRegister.voidLastTransaction;
+	$("button").onclick = cashRegister.applyStaffDiscount;
+	$("discount_btn").onclick = addmember;
+	
+	//Populating staff discount list:
+	for (var x = 0; employees.length; x++) {
+	AddItem(employees[x]._name,employees[x]._name);
+
+}
+
+}
+
+
+
+
+//***************************Cash Register********************************************************
+
+
+
+function StaffMember(_name,discountPercent){
+    this._name = _name;
     this.discountPercent = discountPercent;
 }
 
@@ -9,40 +37,108 @@ var bob = new StaffMember("Bob",10);
 // Create yourself again as 'me' with a staff discount of 20%
 var me = new StaffMember("Osama bin Laden",20);
 
+var employees = [sally,bob,me];
+
+function addmember(){
+	
+    var _name = document.getElementById("emp_name").value;
+    var discount = document.getElementById("emp_discount").value;
+    
+    
+    employees[employees.length] = new StaffMember(_name,discount);
+
+
+	alert("Member successfully added.");
+	
+	AddItem(employees[employees.length-1]._name,employees[employees.length-1]._name);
+	
+	document.getElementById("emp_name").value="";
+	document.getElementById("emp_discount").value="";
+}
+
+
+
 var cashRegister = {
-    total:0,
+    sum:0,
     lastTransactionAmount: 0,
     add: function(itemCost){
-        this.total += (itemCost || 0);
+        this.sum += (itemCost || 0);
         this.lastTransactionAmount = itemCost;
+        
+        document.getElementById("output").innerHTML = "-->$"+this.sum.toFixed(2);
+        
+        
     },
-    scan: function(item,quantity){
+    scan: function(){
+    	
+    		var item = document.getElementById("item_list").value;
+    		var quantity = document.getElementById("item_qty").value;
+    		
         switch (item){
-        case "eggs": this.add(0.98 * quantity); break;
-        case "milk": this.add(1.23 * quantity); break;
-        case "magazine": this.add(4.99 * quantity); break;
-        case "chocolate": this.add(0.45 * quantity); break;
+        case "eggs": cashRegister.add(0.98 * quantity); break;
+        case "milk": cashRegister.add(1.23 * quantity); break;
+        case "magazine": cashRegister.add(4.99 * quantity); break;
+        case "chocolate": cashRegister.add(0.45 * quantity); break;
         }
         return true;
+        
+
     },
     voidLastTransaction : function(){
-        this.total -= this.lastTransactionAmount;
-        this.lastTransactionAmount = 0;
+        cashRegister.sum -= cashRegister.lastTransactionAmount;
+        cashRegister.lastTransactionAmount = 0;
+        document.getElementById("output").innerHTML = "-->$"+ cashRegister.sum;
+       
     },
     // Create a new method applyStaffDiscount here
-    applyStaffDiscount: function(employee){
+    applyStaffDiscount: function(){
         
-        this.total = this.total - (0.01* employee["discountPercent"]*this.total);
+        var sname = document.getElementById("staff_select").value;
+		      
+        cashRegister.sum = cashRegister.sum - (0.01 * discount(sname)  * cashRegister.sum);
+        document.getElementById("output").innerHTML = "-->$"+ cashRegister.sum.toFixed(2);
+        
+        
     }
     
 };
 
-cashRegister.scan('eggs',1);
-cashRegister.scan('milk',1);
-cashRegister.scan('magazine',3);
+//cashRegister.scan('eggs',1);
+//cashRegister.scan('milk',1);
+//cashRegister.scan('magazine',3);
 // Apply your staff discount by passing the 'me' object 
 // to applyStaffDiscount
-cashRegister.applyStaffDiscount(me);
+//cashRegister.applyStaffDiscount(me);
 
 // Show the total bill
-console.log('Your bill is '+cashRegister.total.toFixed(2));
+//console.log('Your bill is '+cashRegister.total.toFixed(2));
+
+ function AddItem(Text,Value)
+
+    {
+    	
+
+        // Create an Option object        
+        var opt = document.createElement("option");
+     		// Add an Option object to Drop Down/List Box
+        document.getElementById("staff_select").options.add(opt);
+        // Assign text and value to Option object
+        opt.text = Text;
+        opt.value = Value;
+
+    }
+    
+    
+var discount = function (empname) {
+	
+	for(var i = 0; i < employees.length; i++){
+			if(employees[i]._name === empname)	{
+			return parseInt(employees[i].discountPercent);
+			}
+	}
+};
+	
+
+
+
+
---
+++ scripta.js
@@ -0,0 +1,127 @@
+window.onload = function () {
+	
+
+	$("listbutton").onclick = list;
+	$("ssearch").onclick = _search;
+	$("addbutton").onclick = add;
+	$("clear").onclick = clearDisplay;
+	
+}
+
+
+/***************************Adress Book**************************************/
+
+
+
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    //console.log(person.firstName + " " + person.lastName);
+    return person.firstName + " " + person.lastName
+}
+
+function list() {
+	
+	document.getElementById("displayparagraph").innerHTML = "";	
+	document.getElementById("displayphone").innerHTML = "";
+	document.getElementById("displayemail").innerHTML = "";
+		
+	
+	var contactsLength = contacts.length;
+	var str = "";
+	for (var i = 0; i < contactsLength; i++) {
+		//printPerson(contacts[i])
+		 str += printPerson(contacts[i])+ "<br/>" ;
+
+	}
+	document.getElementById("displayparagraph").innerHTML = str;
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+
+function _search(){
+	
+	document.getElementById("displayparagraph").innerHTML = "";	
+	document.getElementById("displayphone").innerHTML = "";
+	document.getElementById("displayemail").innerHTML = "";
+	
+	var name = document.getElementById("nameinput").value;
+	var str = "";
+	var phone = "";
+	var email = "";
+    var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		if(name === contacts[i].lastName){
+		//printPerson(contacts[i]);
+		str += printPerson(contacts[i])+ "<br/>" ;
+		phone += contacts[i].phoneNumber + "<br/>" ;
+		phone += contacts[i].email + "<br/>" ;
+
+	}
+	document.getElementById("displayparagraph").innerHTML = str;
+	document.getElementById("displayphone").innerHTML = phone;
+	document.getElementById("displayemail").innerHTML = email;
+	
+	
+	document.getElementById("nameinput").value = "";
+		
+	}
+	
+}
+
+//search("Jones");
+
+
+function add(){
+	
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    
+    
+    contacts[contacts.length] = {
+    firstName: firstname,
+    lastName: lastname,
+    phoneNumber: phone,
+    email: email
+}
+
+ alert("Contact successfully created");
+ 
+ document.getElementById("firstname").value = "";
+ document.getElementById("lastname").value = "";
+ document.getElementById("email").value ="";
+ document.getElementById("phone").value = "";
+ }
+ 
+
+//add("John", "Brown", "email@email.com", "555-0123");
+
+//list();
+
+function clearDisplay(){
+
+	document.getElementById("displayparagraph").innerHTML = "";	
+	document.getElementById("displayphone").innerHTML = "";
+	document.getElementById("displayemail").innerHTML = "";
+	
+	
+}
